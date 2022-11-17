7135589724f8acecb93c34d87cccd74e2f500282
Ishhyoboytarun <prudent.tarun0808@gmail.com>
2022-11-17 21:21:59 +0530

    abstract-factory

--- Factory-design-pattern/abstract_factory/iSuitFactory.go
+++ Factory-design-pattern/abstract_factory/iSuitFactory.go
@@ -5,3 +5,13 @@ type ISuitFactory interface {
 	makePants() IPants
 	makeBlazer() IBlazer
 }
+
+func GetSuitFactory(brand string) (ISuitFactory,error) {
+	if brand == "PeterEngland"{
+		return &PeterEngland{},nil
+	}
+	if brand == "Levis"{
+		return &Levis{},nil
+	}
+	return nil,"Wrong brand"
+}
